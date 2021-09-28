working P controller with wrong diff_angle calculation

src/pack_lab4/src/move_mode.py
@@ -1,23 +1,29 @@
-
-#!/usr/bin/python3
 import rospy
-import math
-# from nav_msgs.msg import Odometry  # for the position
-# from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Twist, Point
+from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import Point, Twist
+from math import atan2
+
+# x = 0.0
+# y = 0.0
+# theta = 0.0
+rospy.init_node("speed_controller")
+pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
 
 
-x = 0.0
-y = 0.0
-theta = 0.0
+def imput_points():
+    global goal
+    goal = Point()
+    goal.x = float(input("Input goal X position: "))
+    goal.y = float(input("Input goal Y position: "))
 
 
-def newOdom(msg):
-    global x
-    global y
-    global theta
-
+def callback(msg):
+    # global x, y, theta, goal
+    global goal
+    # goal.x = 8
+    # goal.y = -3
+    speed = Twist()
     x = msg.pose.pose.position.x
     y = msg.pose.pose.position.y
 
@@ -25,70 +31,33 @@ def newOdom(msg):
     (roll, pitch, theta) = euler_from_quaternion(
         [rot_q.x, rot_q.y, rot_q.z, rot_q.w])
 
+    inc_x = goal.x - x
+    inc_y = goal.y - y
 
-pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-move = Twist()
+    angle_to_goal = atan2(inc_y, inc_x)
 
+    # print(theta, "the current angle")
+    # print(angle_to_goal, "the angles the to points")
 
-def movement_worng(x_curr, y_curr):
-    # move in x
-    if abs(goal_pos_x - x_curr) >= 0.05:
-        # print("first")
-        if (goal_pos_x - x_curr) > 0:
-            move.linear.x = 0.5
-        if (goal_pos_x - x_curr) < 0:
-            move.linear.x = -0.5
+    if abs(angle_to_goal - theta) > 0.4:
+        if (angle_to_goal - theta) > 0.4:
+            speed.linear.x = 0.0
+            speed.angular.z = 0.3
+        if (angle_to_goal - theta) < 0.4:
+            speed.linear.x = 0.0
+            speed.angular.z = -0.3
     else:
-        move.linear.x = 0
-    # move in y
-    if abs(goal_pos_y - y_curr) >= 0.05:
-        # print("second")
-        if (goal_pos_y - y_curr) > 0:
-            move.linear.y = 0.5
-        if (goal_pos_y - y_curr) < 0:
-            move.linear.y = -0.5
-    else:
-        move.linear.y = 0
+        speed.linear.x = 0.5
+        speed.angular.z = 0.0
 
-    # move.linear.x = 1
-    # move.linear.y = 1
-    print(move.linear.x, "movement in x")
-    print(move.linear.y, "movement in y")
-    pub.publish(move)
-
-
-def callback(msg):
-
-    # goal_pos_x = 0
-    # goal_pos_y = 0
-    x_curr = msg.pose.pose.position.x
-    y_curr = msg.pose.pose.position.y
-    print(x_curr, "current x pos")
-    print(y_curr, "current y pos")
-    # print(abs(goal_pos_x - x_curr), "x")
-    # print(abs(goal_pos_y - y_curr), "y")
-    # print(goal_pos_x, "x")
-    # print(goal_pos_y, "y")
-
-    movement_worng(x_curr, y_curr)
+    print(speed.linear.x, "linear")
+    print(speed.angular.z, "angular")
+    pub.publish(speed)
 
 
 def main():
-
-    global goal_pos_x
-    global goal_pos_y
-    goal_pos_x = float(input("input goal x position: "))
-    goal_pos_y = float(input("input goal y position: "))
-    rospy.init_node('move_to_pos', anonymous=True)
-    # global pub
-    # global move
-    # pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-    # move = Twist()
-    # print(goal_pos_x, "x")
-    # print(goal_pos_y, "y")
-    sub = rospy.Subscriber('/odom', Odometry, callback)
-
-    # keeps python form exiting
+    imput_points()
+    sub = rospy.Subscriber("/odom", Odometry, callback, queue_size=1)
     rospy.spin()
 
 