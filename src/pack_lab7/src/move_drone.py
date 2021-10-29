f99fa673223edac2abb6d74c499e401e5ef04b83
Valentin Resapow <valentin.resapow@tuhh.de>
2021-10-29 14:47:03 +0300

    new pack8

--- src/pack_lab7/src/move_drone.py
+++ src/pack_lab7/src/move_drone.py
@@ -25,15 +25,55 @@ import rospy
 
 rospy.init_node("drone_control")
 pub_takeoff = rospy.Publisher("drone/takeoff", Empty, queue_size=1)
+pub_move = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
 pub_land = rospy.Publisher("/drone/land", Empty, queue_size=1)
 hey = Empty()
+movment = Twist()
 
 
 def main():
     time.sleep(0.5)
-    pub_takeoff.publish(hey)
+    while True:
+        action = input(
+            "What should the drone do?\n 1= take off \n 2= land\n 3= move\n")
+        if action == "1":
+            pub_takeoff.publish(hey)
+        if action == "2":
+            pub_land.publish(hey)
+        if action == "3":
+            print("Which direction")
+            while True:
+                move_dir = input()
+                if move_dir == "h":
+                    movment.linear.x = -1
+                    movment.linear.y = 0
+                    print("sidewayse")
+                if move_dir == "j":
+                    movment.linear.x = 0
+                    movment.linear.y = -1
+                    print("forwards")
+                if move_dir == "k":
+                    movment.linear.x = 0
+                    movment.linear.y = 1
+                    print("backwards")
+                if move_dir == "l":
+                    movment.linear.x = 1
+                    movment.linear.y = 0
+                    print("sidewayse")
+                if move_dir == "0":
+                    movment.linear.x = 0
+                    movment.linear.y = 0
+                pub_move.publish(movment)
+                if move_dir == "q":
+                    movment.linear.x = 0
+                    movment.linear.y = 0
+                    pub_move.publish(movment)
+                    break
+        action = "0"
+
     time.sleep(4)
     pub_land.publish(hey)
+
     # sub_inital = rospy.Subscriber(
     #     "/turtlebot/goal_pose", Pose, get_goal_point, queue_size=1)
     # sub = rospy.Subscriber("/odom", Odometry, callback, queue_size=1)
---
+++ src/pack_lab8/src/move_PID_controller.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python3
+
+from geometry_msgs.msg import Pose, Twist, Point
+from tf.transformations import euler_from_quaternion
+from nav_msgs.msg import Odometry
+import math
+import time
+import rospy
+# import PID_controller_class
+
+
+class PID_controller:
+    def __init__(self, kp_v, kp_av, ki_v, ki_av, kd_v, kd_av):
+
+        self._kp_v = kp_v
+        self._kp_av = kp_av
+        self._ki_v = ki_v
+        self._ki_av = ki_av
+        self._kd_v = kd_v
+        self._kd_av = kd_av
+
+        self._ki_v_min = ki_v
+        self._ki_av_min = ki_av
+        self._ki_v_max = ki_v
+        self._ki_av_max = ki_av
+
+        self._last_time = None
+        self.dt = None
+        self._linear_error_last = None
+        self._angular_error_last = None
+
+    def calcualte_time_loop(self):
+        """
+        start loop timing and calculate the time of one loop
+        first_loop -> dt = 0
+        """
+        actual_time = time.time()
+
+        if self._last_time == None:
+            self._last_time = actual_time
+
+        self.dt = actual_time - self._last_time
+        self._last_time = actual_time
+
+    def update_PID_contoller(self, linear_error, angular_error):
+        """
+        linear_error:= x and y difference between actual position and goal position
+        angular_error:= angular difference between actual angel and the angel to the goal
+
+        The actual implementation  of the PID controller.
+
+        """
+
+        self.calcualte_time_loop()
+        if self.dt == 0:
+            self._angular_controler = 0
+            self._linear_controler = 0
+            return self._angular_controler, self._linear_controler
+
+        # P controller part
+        linear_velocity_p = self._kp_v * linear_error
+        angular_velocity_p = self._kp_av * angular_error
+
+        if(self._linear_error_last == None):
+            self._linear_error_last = linear_error
+        if(self._angular_error_last == None):
+            self._angular_error_last = angular_error
+
+        # D controller part
+        linear_velocity_d = self._kd_v * \
+            (self._linear_error_last - linear_error)/self.dt
+        angular_velocity_d = self._kd_av * \
+            (self._linear_error_last - linear_error)/self.dt
+
+        # linear_velocity_i += self.dt*self._kp_v
+
+        self._linear_error_last = linear_error
+        self._angular_error_last = angular_error
+
+        print(linear_velocity_p, "linear_velocity_p")
+        print(linear_velocity_d, "linear_velocity_d")
+
+        print(angular_velocity_p, "angular_velocity_p")
+        print(angular_velocity_d, "angular_velocity_d")
+
+        self._linear_controler = linear_velocity_p + linear_velocity_d
+        self._angular_controler = angular_velocity_p + angular_velocity_d
+
+        # limits speed
+        if abs(self._linear_controler) >= 0.5:  # should not be negative
+            self._linear_controler = 0.5
+
+
+# tuning parameters
+kp_v = 0.2
+kp_av = 0.7
+
+ki_v = 0.5
+ki_av = 0.02
+
+kd_v = 0.5
+kd_av = 0.2
+
+# pub velocity for turtlebot
+rospy.init_node("speed_controller")
+pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+
+
+PID_class = PID_controller(
+    kp_v, kp_av, ki_v, ki_av, kd_v, kd_av)
+
+
+def imput_points():
+    global goal
+    goal = Point()
+    goal.x = float(input("Input goal X position: "))
+    goal.y = float(input("Input goal Y position: "))
+    goal.z = float(input("Input goal Z position: "))
+
+
+def diff_angels(x, y):
+    x += math.pi
+    y += math.pi
+    a = (x - y) % (math.pi*2)
+    b = (y - x) % (math.pi*2)
+    return -a if a < b else b
+
+
+def dist(x, y, z):
+    global goal
+    inc_x = goal.x - x
+    inc_y = goal.y - y
+    inc_z = goal.z - z
+    distance = math.sqrt(inc_x**2 + inc_y**2 + inc_y**2)
+    return inc_x, inc_y, inc_y, distance
+
+
+def get_goal_point(msg):
+    global goal, theta
+    goal.x = msg.Point.x
+    goal.y = msg.Point.y
+    goal.z = msg.Point.z
+    (roll, pitch, theta) = euler_from_quaternion(
+        [rot_q.Quaternion.x, rot_q.Quaternion.y, rot_q.Quaternion.z, rot_q.Quaternion.w])
+
+
+def callback(msg):
+    global goal
+    speed = Twist()
+    x = msg.pose.pose.position.x
+    y = msg.pose.pose.position.y
+    rot_q = msg.pose.pose.orientation
+    (roll, pitch, theta) = euler_from_quaternion(
+        [rot_q.x, rot_q.y, rot_q.z, rot_q.w])
+
+    inc_x, inc_y, distance_to_goal = dist(x, y, z)
+    angle_to_goal = math.atan2(inc_y, inc_x)
+    diff_ang = -diff_angels(angle_to_goal, theta)
+
+    if distance_to_goal > 0.2:
+        PID_class.update_PID_contoller(
+            distance_to_goal, diff_ang)
+
+    speed.linear.x = PID_class._linear_controler
+    speed.angular.z = PID_class._angular_controler
+    # speed.angular.z = 0
+
+    pub.publish(speed)
+
+
+def main():
+    imput_points()
+
+    sub_inital = rospy.Subscriber(
+        "/turtlebot/goal_pose", Pose, get_goal_point, queue_size=1)
+    sub = rospy.Subscriber("/odom", Odometry, callback, queue_size=1)
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
